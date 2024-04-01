4ff644cd36cc63c6013a4d58d95779b8a8514b0c
nurbek <nhorlvick2002@yahoo.com>
2024-04-01 10:24:07 +0800

    changes in layout

--- layout/side-nav.php
+++ layout/side-nav.php
@@ -12,7 +12,7 @@
                     <i class="fas fa-clipboard-user pe-2"></i> Records
                 </a>
                 <div class="collapse" id="recordsSubmenu" data-bs-dismiss="offcanvas">
-                    <a href="#subitem1" class="list-group-item list-group-item-action bs-light tabShow" id="subitem1-tab" aria-expanded="false" role="tab" data-bs-toggle="tab" aria-controls="subitem1">Subitem 1</a>
+                    <a href="?tb=1" data-bs-target="#agenda" class="list-group-item list-group-item-action bs-light tabShow" id="subitem1-tab" aria-expanded="false" role="tab" data-bs-toggle="tab" aria-controls="subitem1">Subitem 1</a>
                     <a href="#subitem2" class="list-group-item list-group-item-action bs-light tabShow" id="subitem2-tab" aria-expanded="false" role="tab" data-bs-toggle="tab" aria-controls="subitem1">Subitem 2</a>
                 </div>
             </li>
--- superadmin/superadmin.php
+++ superadmin/superadmin.php
@@ -94,77 +94,24 @@ if (isset($_SESSION['username']) && $_SESSION['userlevel'] === "superadmin") {
                             <main class="content px-3 py-2">
                                 <div class="row">
                                     <div class="container">
-                                        <div class="col-sm col-md col-lg">
-                                            <div class="d-flex justify-content-end grid mb-3 gap-2 align-items-center">
-                                                <label for="min-date">From:</label>
-                                                <input type="date" class="form-control filter-input" id="min-date">
-                                                <label for="max-date">To:</label>
-                                                <input type="date" class="form-control filter-input" id="max-date">
-                                            </div>
-                                            <div class="">
-                                                <table class="table table-bordered nowrap" style="width: 100%;" id="list-agenda">
-                                                    <thead>
-                                                        <th>No</th>
-                                                        <th>Title</th>
-                                                        <th>Author</th>
-                                                        <th>Date</th>
-                                                        <th class="last-child">Action</th>
-                                                    </thead>
-
-                                                    <tbody>
-                                                        <?php
-                                                        $iteration_query = "SELECT * FROM `e-agenda`";
-                                                        $result = $sqlcon->query($iteration_query);
-
-                                                        while ($rows = mysqli_fetch_assoc($result)) {
-                                                        ?>
-                                                            <tr>
-                                                                <td><?= $rows['agenda_no']; ?></td>
-                                                                <td><?= $rows['title']; ?></td>
-                                                                <td><?= $rows['author']; ?></td>
-                                                                <td><?= $rows['date']; ?></td>
-                                                                <td>
-                                                                    <a href="<?= $rows['filepath'] ?>" target="_blank" class="btn btn-primary">Download</a>
-                                                                    <a data-bs-toggle="modal" data-bs-target="#confirmation" class="btn btn-danger">Delete</a>
-                                                                </td>
-                                                            </tr>
-
-                                                            <!-- Confirmation Modal -->
-                                                            <div class="modal fade" id="confirmation" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                                <div class="modal-dialog modal-dialog-centered">
-                                                                    <div class="modal-content">
-                                                                        <div class="modal-header">
-                                                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Confirm</h1>
-                                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                                        </div>
-                                                                        <div class="modal-body">
-                                                                            <p>Are you sure you want to delete?</p>
-                                                                        </div>
-                                                                        <div class="modal-footer">
-                                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                                            <a href="delete.php?target_id=<?= $rows['id'] ?>" class="btn btn-danger">Delete</a>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                            <!-- End of Modal -->
-
-                                                        <?php  } ?>
-                                                    </tbody>
-
-                                                </table>
-                                            </div>
+                                        <!-- Pane Description -->
+                                        <div class="p-2 mb-3 mt-3">
+                                            <h1 class="h1">Home</h1>
                                         </div>
                                     </div>
                                 </div>
                             </main>
                         </div>
 
-                        <div class="tab-pane fade show" id="subitem1" role="tabpanel" aria-labelledby="subitem1-tab">
+                        <div class="tab-pane fade show" id="agenda" role="tabpanel" aria-labelledby="subitem1-tab">
                             <main class="content px-3 py-2">
-                                <div class="container-fluid">
-                                    <h1 class="mt-4">Subitem 1</h1>
-                                    <p>This is the main content area.</p>
+                                <div class="row">
+                                    <div class="container">
+                                        <h1 class="p-2 mb-3 mt-3">E-Agenda</h1>
+                                    </div>
+                                    <?php
+                                    include("../layout/e-agenda.php");
+                                    ?>
                                 </div>
                             </main>
                         </div>
@@ -245,7 +192,7 @@ if (isset($_SESSION['username']) && $_SESSION['userlevel'] === "superadmin") {
                                     </div>
                             </div>
                             <div class="modal-footer">
-                                <button class="btn btn-primary">Send message</button>
+                                <button class="btn btn-primary">Add Agenda</button>
                                 </form>
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             </div>
