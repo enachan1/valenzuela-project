b3236194e2d7988ea708a82b65d7774828c5d1e8
jekramirez <jek.ramirez.24@gmail.com>
2024-04-18 10:47:17 +0800

    removed components on modal add agenda

--- layout/table-modal-crud/crud-agenda.php
+++ layout/table-modal-crud/crud-agenda.php
@@ -5,21 +5,13 @@
     </div>
     <div class="modal-body">
         <form method="POST" action="upload.php" enctype="multipart/form-data" autocomplete="off">
-            <div class="mb-2">
-                <label for="recipient-name" class="col-form-label">No.</label>
-                <input type="text" class="form-control" name="no" id="">
-            </div>
-            <div class="mb-2">
+            <div class="form-floating mb-2">
+                <input class="form-control" id="message-text" name="title" placeholder="Title"></input>
                 <label for="recipient-name" class="col-form-label">Title</label>
-                <textarea class="form-control" id="message-text" name="title"></textarea>
             </div>
-            <div class="mb-2">
-                <label for="message-text" class="col-form-label">Author</label>
-                <input type="text" class="form-control" name="author" id="auth">
-            </div>
-            <div class="mb-2">
-                <label for="message-text" class="col-form-label">Date</label>
+            <div class="form-floating mb-2">
                 <input type="date" class="form-control" name="date" id="auth">
+                <label for="auth" class="col-form-label">Date</label>
             </div>
             <div class="mb-2">
                 <label for="message-text" class="col-form-label">PDF Upload</label>
